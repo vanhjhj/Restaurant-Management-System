f21458cdfec26c2d052444fdad9215bc2cc068a7
Nguyen Dang Tri <22120383@student.hcmus.edu.vn>
2024-11-06 20:46:35 +0700

    update folder

--- LoginRegister/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Account, CustomerAccount, EmployeeAccount, Department
-
-# Register your models here.
-admin.site.register(Account)
-admin.site.register(CustomerAccount)
-admin.site.register(EmployeeAccount)
-admin.site.register(Department)
