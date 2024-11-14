3ed3fffc4cf77c6d2c7b73ed158abb6905d38b57
Nguyen Dang Tri <22120383@student.hcmus.edu.vn>
2024-11-14 23:09:23 +0700

    fix menuitems model

--- src/Backend/Menu/urls.py
+++ src/Backend/Menu/urls.py
@@ -2,8 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('category/', views.CategoryListCreateAPIView.as_view(), name='category'),
-    path('category/<int:pk>/', views.CategoryRetrieveUpdateDestroyAPIView.as_view(), name='category'),
-    path('menuitem/', views.MenuItemListCreateAPIView.as_view(), name='menuitem'),
-    path('menuitem/<int:pk>/', views.MenuItemRetrieveUpdateDestroyAPIView.as_view(), name='menuitem'),
+    path('categories/', views.CategoryListCreateAPIView.as_view(), name='categories'),
+    path('categories/<int:pk>/', views.CategoryRetrieveUpdateDestroyAPIView.as_view(), name='categories'),
+    path('menuitems/', views.MenuItemListCreateAPIView.as_view(), name='menuitems'),
+    path('menuitems/<int:pk>/', views.MenuItemRetrieveUpdateDestroyAPIView.as_view(), name='menuitems'),
 ]
