a4e3f39135f239aecf48177bf08e9eae92e10d68
Nguyen Dang Tri <22120383@student.hcmus.edu.vn>
2024-11-23 08:35:15 +0700

    fix virtual environment, add .gitignore

--- src/Backend/Restaurant/settings.py
+++ src/Backend/Restaurant/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'LoginRegister',
+    'Menu',
     'rest_framework',
     'phonenumber_field',
     'rest_framework_simplejwt',
