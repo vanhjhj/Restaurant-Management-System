1e6a3be8c27b1c843a295d84960fd0fadae950b8
nhyy204 <22120425@student.hcmus.edu.vn>
2024-12-27 20:26:31 +0700

    Fix show promotiondetail

--- src/Frontend/src/Components/Layout/Promotion/PromotionDetail.js
+++ src/Frontend/src/Components/Layout/Promotion/PromotionDetail.js
@@ -34,22 +34,27 @@ function PromotionDetail() {
   return (
     <div className={style["promotion-detail"]}>
       <h2>{promotion.title}</h2>
-      <img
-        src={promotion.image}
-        alt={promotion.title}
-        className={style["promotion-image"]}
-      />
-      <p className={style["promotion-description"]}>{promotion.description}</p>
-      <p>----------</p>
-      <p className={style["promotion-startdate"]}>
-        Bắt đầu từ: {promotion.startdate}
-      </p>
-      <p className={style["promotion-enddate"]}>
-        Kết thúc vào: {promotion.enddate}
-      </p>
-      <p className={style["promotion-code"]}>
-        Điền mã này để được áp dụng khuyến mãi: {promotion.code}
-      </p>
+      <div className={style["promotion-container"]}>
+        <div className={style["promotion-image"]}>
+          <img src={promotion.image} alt={promotion.title} />
+        </div>
+        <div className={style["promotion-info"]}>
+          <p className={style["promotion-description"]}>
+            {promotion.description}
+          </p>
+          <hr className={style["divider"]} />
+          <p className={style["promotion-startdate"]}>
+            Bắt đầu từ: {promotion.startdate}
+          </p>
+          <p className={style["promotion-enddate"]}>
+            Kết thúc vào: {promotion.enddate}
+          </p>
+          <hr className={style["divider"]} />
+          <p className={style["promotion-code"]}>
+            Điền mã này để được áp dụng khuyến mãi: {promotion.code}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
