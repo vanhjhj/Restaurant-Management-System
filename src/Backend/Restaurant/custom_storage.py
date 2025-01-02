188ed55e07a01dbbc2895d14f5a4d6e93122a2e5
Daniiislo <22120383@student.hcmus.edu.vn>
2025-01-02 11:16:28 +0700

    update StaticStorage

--- src/Backend/Restaurant/custom_storage.py
+++ src/Backend/Restaurant/custom_storage.py
@@ -3,7 +3,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = 'static'
-    default_acl = 'public-read'
+    default_acl = None
+    file_overwrite = False
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'
