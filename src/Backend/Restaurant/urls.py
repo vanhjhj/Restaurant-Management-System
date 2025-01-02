b03a01113d5d301d5ba31f5310377158559cdd41
Daniiislo <22120383@student.hcmus.edu.vn>
2025-01-02 17:21:51 +0700

    add api to config restaurant infomation

--- src/Backend/Restaurant/urls.py
+++ src/Backend/Restaurant/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('api/menu/', include('Menu.urls')),
     path('api/booking/', include('Booking.urls')),
     path('api/promotion/', include('Promotion.urls')),
+    path('api/config/', include('Config.urls')),
 ]
 
